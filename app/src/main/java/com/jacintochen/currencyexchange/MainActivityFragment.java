1f35a2c08a6b75265ed6c54e2071b1dce379e9db
Jacinto <jchen36.jc@gmail.com>
2015-06-23 23:35:38 -0700

    Fixed the button state change. Also tried implementing a functioning button; however, found out that implementing the onClick by xml isn't a good idea since it goes through the mainActivity. Need to fix this.

--- app/src/main/java/com/jacintochen/currencyexchange/MainActivityFragment.java
+++ app/src/main/java/com/jacintochen/currencyexchange/MainActivityFragment.java
@@ -2,9 +2,14 @@ package com.jacintochen.currencyexchange;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 
 /**
@@ -12,12 +17,51 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    private TextView mstart_currency_textView;
+    private TextView mend_currency_textView;
+    private DecimalFormat mnumberFormatter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mstart_currency_textView = (TextView) rootView.findViewById(R.id.start_currency_textview);
+        mend_currency_textView = (TextView) rootView.findViewById(R.id.end_currency_textview);
+
+        //TODO: Allow user to add more than 2 decimal points
+        mnumberFormatter = new DecimalFormat("#,###.##");
+
+        return rootView;
     }
+
+
+    public void addEight(View v){
+        appendNumberHelper("8");
+    }
+
+    public void appendNumberHelper(String num){
+        String number = mstart_currency_textView.getText().toString();
+        String result = null;
+        try {
+            number = Double.toString(mnumberFormatter.parse(number).doubleValue());
+            number += num;
+            Double d = Double.parseDouble(number);
+
+            result = mnumberFormatter.format(d);
+        } catch (Exception ex){
+            Log.v(LOG_TAG, "The parsing caused a problem!");
+            return;
+        }
+
+        if (result != null){
+            mstart_currency_textView.setText(result);
+        }
+    }
+
 }
