7eb0934642a9368db2c95497d6aaf25f58e73867
Jacinto <jchen36.jc@gmail.com>
2015-09-16 22:31:34 -0700

    Made the default data when the app is first installed more robust and clearer.

--- app/src/main/java/com/jacintochen/currencyexchange/data/ExchangeDbHelper.java
+++ app/src/main/java/com/jacintochen/currencyexchange/data/ExchangeDbHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+
 /**
  * Created by Bokii on 7/2/2015.
  */
@@ -14,6 +15,10 @@ public class ExchangeDbHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     static final String DATABASE_NAME = "exchange.db";
+
+    // Default values when starting the app for the first time
+    final String USD_TO_PESO = "'USD', 'PESO', '14', '15', '0.0714', '0.06667'";
+    final String USD_TO_BIGMAC = "'USD', 'BIG MAC', '3.99', '3.99', '0.2506', '0.2506'";
 
     public ExchangeDbHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -30,12 +35,23 @@ public class ExchangeDbHelper extends SQLiteOpenHelper {
                 ExchangeContract.COLUMN_BANK_RATE_TWO_TO_ONE + " TEXT NOT NULL, " +
                 ExchangeContract.COLUMN_MARKET_RATE_TWO_TO_ONE + " TEXT NOT NULL);";
 
-        // TODO: Make default values as constant text in string xml
-        final String SQL_STARTING_USD_PESO = "INSERT INTO " + ExchangeContract.TABLE_NAME +
-                " VALUES(" +
-                "0, 'USD', 'PESO', '14', '15', '0.0714', '0.06667');";
-        final String SQL_STARTING_USD_BIG_MAC = "INSERT INTO " + ExchangeContract.TABLE_NAME + " VALUES(" +
-                "1, 'USD', 'BIG MAC', '3.99', '3.99', '0.2506', '0.2506');";
+        final String SQL_STARTING_USD_PESO = "INSERT INTO " + ExchangeContract.TABLE_NAME + " (" +
+                ExchangeContract.COLUMN_CURRENCY_ONE + "," +
+                ExchangeContract.COLUMN_CURRENCY_TWO + "," +
+                ExchangeContract.COLUMN_BANK_RATE_ONE_TO_TWO + "," +
+                ExchangeContract.COLUMN_MARKET_RATE_ONE_TO_TWO  + "," +
+                ExchangeContract.COLUMN_BANK_RATE_TWO_TO_ONE + "," +
+                ExchangeContract.COLUMN_MARKET_RATE_TWO_TO_ONE + ") " +
+                "VALUES(" + USD_TO_PESO +");";
+
+        final String SQL_STARTING_USD_BIG_MAC = "INSERT INTO " + ExchangeContract.TABLE_NAME + " (" +
+                ExchangeContract.COLUMN_CURRENCY_ONE + "," +
+                ExchangeContract.COLUMN_CURRENCY_TWO + "," +
+                ExchangeContract.COLUMN_BANK_RATE_ONE_TO_TWO + "," +
+                ExchangeContract.COLUMN_MARKET_RATE_ONE_TO_TWO  + "," +
+                ExchangeContract.COLUMN_BANK_RATE_TWO_TO_ONE + "," +
+                ExchangeContract.COLUMN_MARKET_RATE_TWO_TO_ONE + ") " +
+                " VALUES(" + USD_TO_BIGMAC + ");";
 
         db.execSQL(SQL_CREATE_EXCHANGE_TABLE);
         db.execSQL(SQL_STARTING_USD_PESO);
